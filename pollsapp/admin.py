381789c5fc27812a6eb7f51d24c9c77c0673753e
Nima Hashemi <nima.hashemy@gmail.com>
2014-12-27 06:13:18 +0330

    enabled admin side

--- pollsapp/admin.py
+++ pollsapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from pollsapp.models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- pollsapp/models.py
+++ pollsapp/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -7,7 +10,12 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        retrun self.question_text
+        return self.question_text
+
+
+    def was_published_recently(self):
+    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
@@ -16,3 +24,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+        
